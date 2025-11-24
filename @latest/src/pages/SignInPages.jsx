1b2d3a50be4e4b984b704c32cbde70a2e54f2f69
khongquynhhuong <tieuhoctulac05@example.com>
2025-11-24 23:52:17 +0700

    .

---
+++ @latest/src/pages/SignInPages.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header/Header.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import SignIn from "./SignIn.jsx";
+
+export default function SignInPages() {
+  return (
+    <>
+      <Header />
+      <SignIn />
+      <Footer />
+    </>
+  );
+}
+
---
+++ @latest/src/pages/SignUpPages.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header/Header.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import SignUp from "./SignUp.jsx";
+
+export default function SignUpPages() {
+  return (
+    <>
+      <Header />
+      <SignUp />
+      <Footer />
+    </>
+  );
+}
+
